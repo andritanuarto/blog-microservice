9eeced9841c08386c18a8abcd618b9c038149130
Andri Tanuarto <andritanuarto@gmail.com>
2021-11-02 15:08:36 -0700

    Remove comment request use query request instead

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CommentCreate from './CommentCreate';
-import CommentList from './CommentList';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts');
-    
+    const res = await axios.get("http://localhost:4002/posts");
+
     setPosts(res.data);
   };
 
@@ -16,18 +16,21 @@ const PostList = () => {
     fetchPosts();
   }, []);
 
-  const renderedPosts = Object.values(posts).map(post => {
+  const renderedPosts = Object.values(posts).map((post) => {
     return (
-      <div className="card" style={{ width: '30%', marginBottom: 20 }} key={post.id}>
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
-          <CommentCreate postId={post.id}/>
+          <CommentList comments={post.comments} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
   });
-
 
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
